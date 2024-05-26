299855a875f744f1ae90d5dc268d5bbb06606942
noel.fredrick <noel.fredrick@cheersin.com>
2024-05-26 22:48:26 +0530

    feat: changes

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-// export default defineConfig({
-//   plugins: [react()],
-// })
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' 
-  }
-});
+})
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist' 
+//   }
+// });
